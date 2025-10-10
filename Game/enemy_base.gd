58e7660c3bf2c87449e56825cccaccc80f219e82
Nicholas Elpedes <107874690+nick-elpedes@users.noreply.github.com>
2025-10-10 11:39:39 -0400

    refactor: make player and enemy variables and functions consistent

--- Game/enemy_base.gd
+++ Game/enemy_base.gd
@@ -2,44 +2,50 @@ class_name EnemyBase extends CharacterBody2D
 	#Used to load enemy instances
 const enemy_scene: PackedScene = preload("res://Game/enemy_base.tscn")
 
-@export var health = 1.0	# Damage enemy can take before despawning
-@export var speed = 0.0	# Movement speed
-@export var atkspeed = 2.0	# Time between each bullet fired
-var texture
+var health : float			# Damage enemy can take before despawning
+var speed : int				# Movement speed
+var shot_speed : int			# Speed of shots
+var bullet_lifetime : float	# Bullet lifetime (in seconds)
+var firerate : float			# Cooldown between shots
+var damage : float			# Damage bullets do
 
 	# Insantiates and fires a bullet at the player whenever its attack cooldown expires
 func _on_attack_cooldown_timeout() -> void:
-	var bullet_instance = Bullet.new_bullet(100, Vector2.LEFT, 5, 1, false)
+	var bullet_instance = Bullet.new_bullet(speed, Vector2.LEFT, bullet_lifetime, damage, false)
 	get_parent().add_child(bullet_instance)
-	bullet_instance.position = position + Vector2(-50, 0)
+	bullet_instance.position = position
 	bullet_instance.fire()
 	
 
 	# Called on instantiation to set the visual sprite of the enemy
-func set_sprite(texture: String) -> void:
-	$EnemySprite.texture = texture
+func set_sprite(sprite: Sprite2D) -> void:
+	$EnemySprite.texture = sprite
 
 	# Called on instantiation to set the attack speed of the enemy
-func set_cooldown(time: int) -> void:
+func set_cooldown(time: float) -> void:
 	$Cooldown.wait_time = time
 
 	# Creates and returns a new enemy instance
 @warning_ignore("shadowed_variable")
-static func new_enemy(health: int, speed: int, atkspeed: int, sprite: String) -> CharacterBody2D:
+static func new_enemy(_sprite: Sprite2D = null, health: float = 5.0, speed: int = 200, firerate: float = 0.5, damage: float = 1.0, shot_speed: int = 200, bullet_lifetime: float = 2.0) -> CharacterBody2D:
 	var enemy_instance = enemy_scene.instantiate()
 	enemy_instance.health = health
 	enemy_instance.speed = speed
-	enemy_instance.set_sprite(sprite)
-	enemy_instance.set_cooldown(atkspeed)
+	enemy_instance.firerate = firerate
+	enemy_instance.shot_speed = shot_speed
+	enemy_instance.bullet_lifetime = bullet_lifetime
+	enemy_instance.damage = damage
+	# enemy_instance.set_sprite(sprite)	# This will not work as Sprite2D cannot be a String
+	enemy_instance.set_cooldown(firerate)
 	return enemy_instance
 
 func _ready() -> void:
-	set_cooldown(atkspeed)
+	set_cooldown(firerate)
 	
 func death() -> void:
 	queue_free()
-	
-func hit(damage: float) -> void:
-	health -= damage
+
+func hit(damage_dealt: float) -> void:
+	health -= damage_dealt
 	if (health <= 0):
 		death()
