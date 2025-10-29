6a6af469cf18061edfe269e68283849f328e91ff
Nicholas Elpedes <107874690+nick-elpedes@users.noreply.github.com>
2025-10-29 18:20:33 -0400

    fix: revert leftover changes to enemy base in enemy slime
    wtf i thought i fixed it

--- Scripts/Enemy/enemy_slime.gd
+++ Scripts/Enemy/enemy_slime.gd
@@ -3,7 +3,7 @@ class_name EnemySlime extends EnemyBase
 const enemy_slime_scene: PackedScene = preload("res://Game/Enemy/enemy_slime.tscn")
 
 @warning_ignore("shadowed_variable")
-static func new_enemy(texture: String = "res://Art/Enemy/target.png", health: float = 5.0, speed: int = 10, firerate: float = 2.0, damage: float = 1.0, shot_speed: int = 0, bullet_lifetime: float = 4.0) -> CharacterBody2D:
+static func new_enemy(_sprite = null, health: float = 5.0, speed: int = 10, firerate: float = 2.0, damage: float = 1.0, shot_speed: int = 0, bullet_lifetime: float = 4.0) -> CharacterBody2D:
 	var enemy_instance = enemy_slime_scene.instantiate()
 	enemy_instance.health = health
 	enemy_instance.speed = speed
@@ -11,7 +11,7 @@ static func new_enemy(texture: String = "res://Art/Enemy/target.png", health: fl
 	enemy_instance.shot_speed = shot_speed
 	enemy_instance.bullet_lifetime = bullet_lifetime
 	enemy_instance.damage = damage
-	enemy_instance.set_cooldown()
+	enemy_instance.set_cooldown(firerate)
 	return enemy_instance
 
 # Currently using this to get the position of the player so the enemy can path to them
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_attack_cooldown_timeout() -> void:
-	var bullet_instance = Bullet.new_bullet(shot_speed, Vector2(0,0), bullet_lifetime, damage, false)
+	var bullet_instance = Bullet.new_bullet(shot_speed, Vector2(0,0), bullet_lifetime, damage, false, Globals.bullet_types["default"]["sprite"], Globals.bullet_types["default"]["collision_body"])
 	get_parent().add_child(bullet_instance)
 	bullet_instance.position = position
 	bullet_instance.fire()
