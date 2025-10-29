98f9c9500a5e4a175fd66301a1900960fcb9a9c8
Nicholas Elpedes <107874690+nick-elpedes@users.noreply.github.com>
2025-10-29 17:59:51 -0400

    fix: do not change enemy base
    Do not change enemy base

--- Scripts/Enemy/enemy_base.gd
+++ Scripts/Enemy/enemy_base.gd
@@ -11,18 +11,23 @@ var damage : float			# Damage bullets do
 
 	# Insantiates and fires a bullet at the player whenever its attack cooldown expires
 func _on_attack_cooldown_timeout() -> void:
-	var bullet_instance = Bullet.new_bullet(speed, Vector2.LEFT, bullet_lifetime, damage, false)
+	var bullet_instance = Bullet.new_bullet(speed, Vector2.LEFT, bullet_lifetime, damage, false, Globals.bullet_types["default"]["sprite"], Globals.bullet_types["default"]["collision_body"])
 	get_parent().add_child(bullet_instance)
 	bullet_instance.position = position
 	bullet_instance.fire()
+	
+
+	# Called on instantiation to set the visual sprite of the enemy
+func set_sprite(sprite: Sprite2D) -> void:
+	$EnemySprite.texture = sprite
 
 	# Called on instantiation to set the attack speed of the enemy
-func set_cooldown() -> void:
-	$Cooldown.wait_time = firerate
+func set_cooldown(time: float) -> void:
+	$Cooldown.wait_time = time
 
 	# Creates and returns a new enemy instance
 @warning_ignore("shadowed_variable")
-static func new_enemy(texture: String = "res://Art/Enemy/target.png", health: float = 5.0, speed: int = 200, firerate: float = 2.0, damage: float = 1.0, shot_speed: int = 200, bullet_lifetime: float = 2.0) -> CharacterBody2D:
+static func new_enemy(_sprite: Sprite2D = null, health: float = 5.0, speed: int = 200, firerate: float = 0.5, damage: float = 1.0, shot_speed: int = 200, bullet_lifetime: float = 2.0) -> CharacterBody2D:
 	var enemy_instance = enemy_scene.instantiate()
 	enemy_instance.health = health
 	enemy_instance.speed = speed
@@ -30,9 +35,12 @@ static func new_enemy(texture: String = "res://Art/Enemy/target.png", health: fl
 	enemy_instance.shot_speed = shot_speed
 	enemy_instance.bullet_lifetime = bullet_lifetime
 	enemy_instance.damage = damage
-	enemy_instance.set_cooldown()
-	enemy_instance.get_node("EnemySprite").texture = load(texture)
+	# enemy_instance.set_sprite(sprite)	# This will not work as Sprite2D cannot be a String
+	enemy_instance.set_cooldown(firerate)
 	return enemy_instance
+
+func _ready() -> void:
+	set_cooldown(firerate)
 	
 func death() -> void:
 	queue_free()
