2f5693288bcf77f20394a6fab821635557908fb9
Nicholas Elpedes <107874690+nick-elpedes@users.noreply.github.com>
2025-11-05 17:18:32 -0500

    feat/wip: make new rooms

---
+++ Scripts/Room/room_handler.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Hardcoded for now, rooms can be level specific once we are further in development
+
+@onready var starting_room = load("res://Game/Room/starting_room.tscn")
+@onready var room1 = load("res://Game/Room/room_1.tscn")
+@onready var room2 = load("res://Game/Room/room_2.tscn")
+@onready var room3 = load("res://Game/Room/room_3.tscn")
