c88637f9e2d11a24bfef8681c3ed40baa31edad8
Colin Watson <146386498+Colinwat@users.noreply.github.com>
2025-11-18 16:29:41 -0500

    feat: Evil snake done
    Evil snake is done. It will wait for one second, then moves slowly towards the player and shooting wavy bullets at them every 0.75 seconds for 2.5 seconds. It is done for the playtest, but I may try to change the bullets to move a little slower when curving and faster when moving straight

--- Scripts/Enemy/evil_snake.gd
+++ Scripts/Enemy/evil_snake.gd
@@ -2,10 +2,19 @@ class_name EvilSnake extends EnemyBase
 const enemy_snake_scene: PackedScene = preload("res://Game/Enemy/evil_snake.tscn")
 
 var attacking : bool
-var bullet_direction : int = 1 # determines whether the bullet moves to the left or right first
+var wavy_expression : Expression
+var bullet_direction : int = 1
+var speedy_expression : Expression
+var speedy_expression_args : Array = []
+
+func _ready() -> void:
+	wavy_expression = Expression.new()
+	wavy_expression.parse("sin(10 * find_child(\"Lifetime\").get_time_left()) * x", ["x"])
+	speedy_expression = Expression.new()
+	speedy_expression.parse("0")
 
 @warning_ignore("shadowed_variable_base_class", "shadowed_variable")
-static func new_enemy(_sprite: Sprite2D = null, health: float = 6.0, speed: int = 10, firerate: float = 2, damage: float = 1.0, shot_speed: int = 120, bullet_lifetime: float = 2.0) -> CharacterBody2D:
+static func new_enemy(_sprite: Sprite2D = null, health: float = 6.0, speed: int = 10, firerate: float = 2, damage: float = 1.0, shot_speed: int = 120, bullet_lifetime: float = 4.0) -> CharacterBody2D:
 	var enemy_instance = enemy_snake_scene.instantiate()
 	enemy_instance.health = health
 	enemy_instance.speed = speed
@@ -24,27 +33,33 @@ func find_player() -> Vector2:
 		return player[0].global_position
 	else:
 		return Vector2(0,0)
+	
 
 func _physics_process(delta: float) -> void:
 	if(attacking):
 		var player_direction = find_player() - global_position
 		velocity = player_direction * speed * delta
 		move_and_slide()
+	
 
-	# Insantiates and fires a bullet at the player whenever its attack cooldown expires
 func _on_attack_cooldown_timeout() -> void:
-	$Cooldown.set_paused(true)
+	$Cooldown.stop()
 	$Attacking.start()
+	attacking = true
 	
 	get_tree().create_timer(2.5).timeout.connect(attack_over)
 	
-	var bullet_instance = Bullet.new_bullet(shot_speed, Vector2.LEFT, bullet_lifetime, damage, false, bullet_resource)
+
+func attack_over() -> void:
+	$Cooldown.start()
+	$Attacking.stop()
+	attacking = false
+	
+
+func _on_attacking_timeout() -> void:
+	var bullet_instance = Bullet.new_bullet_wavy_speedy(shot_speed, get_tree().get_first_node_in_group("player").global_position, bullet_lifetime, damage, false, bullet_resource, wavy_expression, [bullet_direction], speedy_expression, speedy_expression_args)
 	get_parent().add_child(bullet_instance)
 	bullet_instance.position = position
 	bullet_instance.fire()
 	
-func attack_over() -> void:
-	$Cooldown.start()
-	$Attacking.pause()
-	
-	attacking = true;
+	bullet_direction *= -1
